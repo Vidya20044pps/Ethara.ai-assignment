07fe0e88cf8af821990227b3196959b1f88023e7
Vidya20044pps <vidya2004pps@gmail.com>
2026-05-01 10:18:21 +0530

    final for deployment

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -18,22 +19,28 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/test", testRoutes);
 app.use("/api/projects", projectRoutes);
-app.use("/api/tasks", taskRoutes); 
+app.use("/api/tasks", taskRoutes);
 app.use("/api/dashboard", dashboardRoutes);
+
 // Test Route
 app.get("/", (req, res) => {
   res.send("API Running Successfully");
 });
 
+// PORT
+const PORT = process.env.PORT || 5000;
+
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
+
     console.log("MongoDB Connected");
 
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
+
   })
   .catch((error) => {
     console.log(error);
